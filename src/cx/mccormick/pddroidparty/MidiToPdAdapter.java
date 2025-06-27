284d23dec0b846dc18645211f53546c7d140bccf
Antoine Rousseau <_antoine_@metalu.net>
2025-06-27 19:19:48 +0200

    Midi input: handle any type of message

--- src/cx/mccormick/pddroidparty/MidiToPdAdapter.java
+++ src/cx/mccormick/pddroidparty/MidiToPdAdapter.java
@@ -7,29 +7,80 @@ import org.puredata.core.PdBase;
 import java.util.ArrayList;
 
 public class MidiToPdAdapter extends MidiReceiver {
-	ArrayList<Integer> message = new ArrayList<Integer>();
+	private static enum State {
+		NOTE_OFF, NOTE_ON, POLY_TOUCH, CONTROL_CHANGE, PROGRAM_CHANGE, AFTERTOUCH, PITCH_BEND, NONE
+	}
+	private State midiState = State.NONE;
+	private int channel;
+	private int firstByte;
+
 	@Override
 	public void onSend(byte[] msg, int offset, int count, long timestamp) {
-		while(count-- != 0) {
-			int b = msg[offset++] & 0xff;
-			if(b > 127) {
-				message.clear();
-				message.add(b);
-				continue;
+		while(count-- != 0) processByte(msg[offset++]);
+	}
+
+	// adapted from https://github.com/nettoyeurny/btmidi/blob/master/AndroidMidi/src/com/noisepages/nettoyeur/midi/FromWireConverter.java
+	private void processByte(int b) {
+		if (b < 0) {
+			midiState = State.values()[(b >> 4) & 0x07];
+			if (midiState != State.NONE) {
+				channel = b & 0x0f;
+				firstByte = -1;
+			} else {
+				PdBase.sendMidiByte(0, b);
 			}
-			message.add(b);
-			if(message.size() == 3) {
-				int status = (message.get(0) >> 4);
-				Log.d("Midi", "message status: " + status + " bytes: " + message.get(1) + " " + message.get(2));
-				if(status == 11) {
-					int channel = message.get(0) & 0x0f;
-					int controller = message.get(1);
-					int value = message.get(2);
-					//Log.d("Midi", "control chan: " + channel + " ctl: " + controller + " val: " + value);
-					PdBase.sendControlChange(channel, controller, value);
+		} else {
+			switch (midiState) {
+			case NOTE_OFF:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendNoteOn(channel, firstByte, 0);
+					firstByte = -1;
 				}
+				break;
+			case NOTE_ON:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendNoteOn(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case POLY_TOUCH:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendPolyAftertouch(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case CONTROL_CHANGE:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendControlChange(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case PROGRAM_CHANGE:
+				PdBase.sendProgramChange(channel, b);
+				break;
+			case AFTERTOUCH:
+				PdBase.sendAftertouch(channel, b);
+				break;
+			case PITCH_BEND:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendPitchBend(channel, ((b << 7) | firstByte) - 8192);
+					firstByte = -1;
+				}
+				break;
+				default /* State.NONE */:
+				PdBase.sendMidiByte(0, b);
+				break;
 			}
-			// TODO: handle other MIDI messages
 		}
 	}
 }
