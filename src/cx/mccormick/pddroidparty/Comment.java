edc3daa2b7dae67d5cbb0a6fd3740dd461c8c62d
Antoine Rousseau <_antoine_@metalu.net>
2025-12-14 14:09:16 +0100

    fix Comment placement

--- src/cx/mccormick/pddroidparty/Comment.java
+++ src/cx/mccormick/pddroidparty/Comment.java
@@ -1,12 +1,16 @@
 package cx.mccormick.pddroidparty;
 
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class Comment extends Widget {
 	private static final String TAG = "Comment";
 
 	public Comment(PdDroidPatchView app, String[] atomline) {
 		super(app);
+
+		Rect tRect = new Rect();
+		paint.getTextBounds("0y", 0, 2, tRect);
 
 		// create the comment string
 		StringBuffer buffer = new StringBuffer();
@@ -18,8 +22,8 @@ public class Comment extends Widget {
 		}
 
 		label = buffer.toString();
-		labelpos[0] = Float.parseFloat(atomline[2]) ;
-		labelpos[1] = Float.parseFloat(atomline[3]) ;
+		labelpos[0] = Float.parseFloat(atomline[2]) + 2;
+		labelpos[1] = Float.parseFloat(atomline[3]) + tRect.height();
 	}
 
 	public void draw(Canvas canvas) {
